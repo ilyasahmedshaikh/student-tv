d0121f9af83608b40638018fa8390be05c950c51
ilyas ahmed <ilyasahmed081@gmail.com>
2021-04-01 17:09:36 +0500

    auth module added with login signup forgot components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  // {
-  //   path: '',
-  //   loadChildren: () => import('./components/auth/auth.module').then(m => m.AuthModule)
-  // },
-  // {
-  //   path: 'auth',
-  //   loadChildren: () => import('./components/auth/auth.module').then(m => m.AuthModule)
-  // },
+  {
+    path: '',
+    loadChildren: () => import('./components/auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./components/auth/auth.module').then(m => m.AuthModule)
+  },
 ];
 
 @NgModule({
