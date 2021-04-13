940bf4cc66b132ba7bd3757d0aaa36b9799a45e9
ilyas ahmed <ilyasahmed081@gmail.com>
2021-04-14 01:11:32 +0500

    fixes colors, tabs capability in course details

--- src/app/components/course-management/course-details/course-details.component.ts
+++ src/app/components/course-management/course-details/course-details.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseDetailsComponent implements OnInit {
 
+  tab: any = 1;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  opentab(num: any) {
+    this.tab=num;
+}
 
 }
