22c4d15c7edd4887e2a0e61cff2c47a7b57b4cd4
muhammadidreesshaikh <idreesiqbal104@gmail.com>
2021-04-16 18:48:19 +0500

    new chat screen done

--- src/app/components/message/new-chat/new-chat.component.ts
+++ src/app/components/message/new-chat/new-chat.component.ts
@@ -7,9 +7,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewChatComponent implements OnInit {
 
+  data: any = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.data = [
+      {
+        image: '../../../../assets/img/man.jpg',
+        name: 'Oliver',
+        available: 'Available'
+      },
+      {
+        image: '../../../../assets/img/profile.png',
+        name: 'William',
+        available: 'Available'
+      },
+      {
+        image: '../../../../assets/img/man.jpg',
+        name: 'James',
+        available: 'Offline'
+      },
+      {
+        image: '../../../../assets/img/profile.png',
+        name: 'Lucas',
+        available: 'Available'
+      },
+      {
+        image: '../../../../assets/img/man.jpg',
+        name: 'Mason',
+        available: 'Offline'
+      },
+      {
+        image: '../../../../assets/img/profile.png',
+        name: 'Ethan',
+        available: 'Available'
+      },
+      {
+        image: '../../../../assets/img/man.jpg',
+        name: 'Alexander',
+        available: 'Offline'
+      },
+      {
+        image: '../../../../assets/img/profile.png',
+        name: 'Logan',
+        available: 'Available'
+      },
+      {
+        image: '../../../../assets/img/man.jpg',
+        name: 'Jackson',
+        available: 'Offline'
+      }
+    ]
   }
 
 }
