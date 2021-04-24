974d766924fdbeb7c8cd1bc49ceb4164f1f69c0d
muhammadidreesshaikh <idreesiqbal104@gmail.com>
2021-04-25 01:23:08 +0500

    create course screen in working

--- src/app/components/course-management/create-course/create-course.component.ts
+++ src/app/components/course-management/create-course/create-course.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { ConfigService } from '../../../core/http/config/config.service';
 
 @Component({
   selector: 'app-create-course',
@@ -9,9 +12,13 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class CreateCourseComponent implements OnInit {
 
   programForm: any = FormGroup;
+  endpoint: any = this.config.API_BASE_URL + '/api/courses';
 
   constructor(
     private fb: FormBuilder,
+    private http: HttpClient,
+    private router: Router,
+    private config: ConfigService,
   ) { }
 
   ngOnInit(): void {
@@ -32,6 +39,31 @@ export class CreateCourseComponent implements OnInit {
       name: ['', Validators.required],
       price: ['', Validators.required],
     });
+  }
+
+  createRegister() {
+    let data = {
+      "id": 0,
+      'active': this.programForm.value.active,
+      'approval': this.programForm.value.approval,
+      'creationDate': new Date(this.programForm.value.date),
+      'creator': this.programForm.value.creator,
+      'description': this.programForm.value.description,
+      'github': this.programForm.value.GithubLink,
+      'level': this.programForm.value.level,
+      'link': this.programForm.value.link,
+      'name': this.programForm.value.name,
+      'price': this.programForm.value.price,
+    };
+
+    this.http.post(this.endpoint, data).subscribe((res: any) => {
+      if (res) {
+        console.log(res);
+      }
+    },
+    (error) => {
+      alert(error.message);
+    })
   }
 
 }
