1f35028e5b9ce68d3939459d9a0060db4aaaf780
muhammadidreesshaikh <idreesiqbal104@gmail.com>
2021-04-25 02:36:21 +0500

    reset password api integrated

--- src/app/components/auth/reset-password/reset-password.component.ts
+++ src/app/components/auth/reset-password/reset-password.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { ConfigService } from '../../../core/http/config/config.service';
+import { PresentationalService } from '../../../core/services/presentational/presentational.service';
 
 @Component({
   selector: 'app-reset-password',
@@ -8,16 +12,55 @@ import { Router } from '@angular/router';
 })
 export class ResetPasswordComponent implements OnInit {
 
+  programForm: any = FormGroup;
+  endpoint: any = this.config.API_BASE_URL + '/resetpassword';
+
   data: any = {};
 
   constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
     private router: Router,
+    private config: ConfigService,
+    private presentationalS: PresentationalService,
   ) {
     this.data = this.router.getCurrentNavigation()?.extras?.state?.data;
   }
 
   ngOnInit(): void {
-    console.log(this.data);
+    this.presentationalS.setPresentation('header', false);
+    this.presentationalS.setPresentation('bottomBar', false);
+
+    this.formInit();
+
+    this.programForm.patchValue({
+      email: this.data.email
+    })
+  }
+
+  formInit() {
+    this.programForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+    });
+  }
+
+  resetPassword() {
+    let data = {
+      'userName': this.programForm.value.email,
+      'password': this.programForm.value.password,
+    };
+
+    this.http.post(this.endpoint, data).subscribe((res: any) => {
+      if (res) {
+        console.log(res);
+      }
+    },
+    (error) => {
+      alert(error.error.text);
+      this.router.navigateByUrl('/auth/login');
+    })
   }
 
 }
