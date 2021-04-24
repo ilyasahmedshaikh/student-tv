faf21d4c9ac0d7d3ad5a9e19845e1660b98942ee
muhammadidreesshaikh <idreesiqbal104@gmail.com>
2021-04-24 16:16:03 +0500

    some fixes and forgot screen in Api

--- src/app/components/auth/forgot/forgot.component.ts
+++ src/app/components/auth/forgot/forgot.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { ConfigService } from '../../../core/http/config/config.service';
+import { PresentationalService } from '../../../core/services/presentational/presentational.service';
+
 
 @Component({
   selector: 'app-forgot',
@@ -7,9 +13,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotComponent implements OnInit {
 
-  constructor() { }
+  programForm: any = FormGroup;
+  endpoint: any = this.config.API_BASE_URL + '/forgotpassword';
+
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private router: Router,
+    private config: ConfigService,
+    private presentationalS: PresentationalService,
+  ) { }
 
   ngOnInit(): void {
+    this.presentationalS.setPresentation('header', false);
+    this.presentationalS.setPresentation('bottomBar', false);
+
+    this.formInit();
+  }
+
+  formInit() {
+    this.programForm = this.fb.group({
+      email: ['', Validators.required],
+    });
+  }
+
+  forgot() {
+    let data = {
+      'userName': this.programForm.value.email,
+    };
+
+    this.http.post(this.endpoint, data).subscribe((res: any) => {
+      if (res) {
+        console.log(res);
+      }
+    },
+    (error) => {
+      alert(error.message);
+    })
   }
 
 }
